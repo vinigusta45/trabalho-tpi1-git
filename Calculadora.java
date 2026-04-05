99da50ca1e3484d19137f5154c561aa89bce17ad
vinigusta45 <vinigusta45@gmail.com>
2026-04-05 14:09:50 -0300

    colocando comentario

--- Calculadora.java
+++ Calculadora.java
@@ -14,6 +14,7 @@ public class Calculadora {
         }
     }
 
+    // Adicionando comentário para revisão do professor, feito por Vinícius
     // Método principal para testar
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
